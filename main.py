e41bfa606169d0e43723558f2e1d42ab40ec76e0
ChickenHawkXP <47929127+ChickenHawkXP@users.noreply.github.com>
2020-01-22 13:03:01 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,214 @@
 import os
 import sys
+import xlsxwriter
+import time
+import datetime
 
-os.system("pdf2txt -o workpdf.txt -F 1.0 -L 2.0 C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser\\test.pdf")
+date = "test"#datetime.date.today()
 
-with open("workpdf.txt","r") as txtfile:
-    text = txtfile.read()
+finalad = list()
+finaltown = list()
+finalnyad = list()
+finalnytown = list()
+#Function that takes a list, and fixes addresses that get separated by \n
+def fixlist(adlist):
+    for i in range(len(adlist)):
+        try:
+            if adlist[i].find(" ") == -1 or adlist[i].find(" ",len(adlist[i])-1) != -1:
+                adlist[i] = adlist[i] + adlist[i+1]
+                adlist.pop(i+1)
+        except IndexError:
+            break
+    return adlist
 
-splittxt = text.split("Featured properties")
+def fixtown(townlist):
+    for i in range(len(townlist)):
+        try:
+            if townlist[i] == "North ":
+                townlist[i] = townlist[i] + townlist[i+1]
+                townlist.pop(i+1)
+        except IndexError:
+            break
+    return townlist
 
-streetad = list()
-towns = list()
+#Function being used to write into columns in excel
+def write_excel(lista,listb):#,nylista,nylistb):
+    headers = ["Full Address","Real Address","Town","State","ZIP","Name"]
+    workbook = xlsxwriter.Workbook("%s.xlsx" %date)
+    worksheet = workbook.add_worksheet()
+    count = 2
+    worksheet.write_row("A1",headers)
+    for i in range(len(lista)):
+        worksheet.write("B%d" %count,"%s"%lista[i])
+        worksheet.write("C%d" %count,"%s"%listb[i])
+        worksheet.write("F%d" %count,"Current Resident")
+        worksheet.write("A%d" %count,"%s %s"%(lista[i],listb[i]))
+        worksheet.write("D%d" %count,"CT")
+        count = count + 1
+    '''
+    for i in range(len(nylista)):
+        worksheet.write("B%d" %count,"%s"%nylista[i])
+        worksheet.write("C%d" %count,"%s"%nylistb[i])
+        worksheet.write("F%d" %count,"Current Resident")
+        worksheet.write("A%d" %count,"%s %s"%(nylista[i],nylistb[i]))
+        worksheet.write("D%d" %count,"NY")
+        count = count+1
+    '''
+    print("Addresses Successfully Loaded into Excel!")
+    workbook.close()
 
-for i in range(len(splittxt)):
-    try:
-        addressidx = splittxt[i].index("Address")
-        style = splittxt[i].index("Presented By")
-    except ValueError:
-        break
-    textrange = splittxt[i][addressidx:style]
+files = os.listdir("C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser")
+hotsheets = list()
+cmas = list()
 
-    adrange = textrange.splitlines()
+for pdfs in files: #Get each file that ends .PDF
+    if pdfs.find("Hot_") != -1:
+        hotsheets.append(pdfs)
+    if pdfs.find("CMA_") != -1:
+        cmas.append(pdfs)
+    
 
-    try:
+for readpdf in range(len(hotsheets)): #Loop through the amount of hotsheets
+    os.system("pdf2txt -o workpdf.txt -F 1.0 -L 2.0 C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser\\%s" %hotsheets[readpdf])
+
+    with open("workpdf.txt","r") as txtfile:
+        text = txtfile.read()
+
+    splittxt = text.split("Featured properties") #Split text by the bottom of each page
+
+    streetad = list()
+
+    print(hotsheets[readpdf])
+    for i in range(len(splittxt)): #Loop throught the splits
+        try:
+            addressidx = splittxt[i].index("Address") # Find "Address" list 
+            style = splittxt[i].index("Presented By") # Find "Presented By" list 
+        except ValueError:
+            break
+
+        textrange = splittxt[i][addressidx:style]
+        adrange = textrange.splitlines()
+
         rangeidx = adrange.index("Address")
-        stopidx = adrange.index('')
-    except ValueError:
-        continue
-    
-    for j in adrange[rangeidx:stopidx]:
-        if j == "Address":
-             continue
-        streetad.append(j)
-        if j.find(" ") == -1 or len(j) < 6:
+
+        try:
+            stopidx = adrange.index('')
+        except ValueError:
+            stopidx = len(adrange)
+        
+        for j in adrange[rangeidx:stopidx]:
+            if j == "Address":
+                continue
+            streetad.append(j)
             index = streetad.index(j)
-            streetad[index-1] = streetad[index-1] + j
-            streetad.pop(index)
 
-for i in range(len(splittxt)):
-    try:
-        addressidx = splittxt[i].index("Town")
-        style = splittxt[i].index("Presented By")
-    except ValueError:
-        break
-    textrange = splittxt[i][addressidx:style]
+    newad = fixlist(streetad)
+    towns = list()
+    for i in range(len(splittxt)):
+        
+        try:
+            addressidx = splittxt[i].index("Town\n")
+            style = splittxt[i].index("Presented By")
+        except ValueError:
+            break
 
-    adrange = textrange.splitlines()
-    try:
+        textrange = splittxt[i][addressidx:style]
+
+        adrange = textrange.splitlines()
         rangeidx = adrange.index("Town")
-        stopidx = adrange.index('')
-    except ValueError:
-        continue
+
+        try:
+            stopidx = adrange.index('')
+        except ValueError:
+            stopidx = len(adrange)
+
+        for j in adrange[rangeidx:stopidx]:
+            if j == "Town":
+                continue
+            towns.append(j)
+
+    newtowns = fixtown(towns)
+    print(newad)
+    print(newtowns)
+    print(len(newad))
+    print(len(newtowns))
+
+    for ads in newad:
+        finalad.append(ads)
+    for ftowns in newtowns:
+        finaltown.append(ftowns)
+ctcount = len(finalad)+len(finaltown)
+    #os.remove("C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser\\%s" %hotsheets[readpdf])
+
+"""
+for readcma in range(len(cmas)): #Loop through the amount of cmas
+    os.system("pdf2txt -o workpdf.txt C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser\\%s" %cmas[readcma])
+
+    with open("workpdf.txt","r") as txtfile:
+        text = txtfile.read()
+
+    splittxt = text.split("Information deemed") #Split text by the bottom of each page
+
+    nystreetad = list()
+
+    print(cmas[readcma])
+    for i in range(len(splittxt)): #Loop throught the splits
+        try:
+            addressidx = splittxt[i].index("Address") # Find "Address" list 
+            style = splittxt[i].index("© 2020 Hudson") # Find "© 2020 Hudson" list 
+        except ValueError:
+            break
+
+        textrange = splittxt[i][addressidx:style]
+        adrange = textrange.splitlines()
+
+        rangeidx = adrange.index("Address")
+
+        try:
+            stopidx = adrange.index('')
+        except ValueError:
+            stopidx = len(adrange)
+        
+        for j in adrange[rangeidx:stopidx]:
+            if j == "Address":
+                continue
+            nystreetad.append(j)
+            index = nystreetad.index(j)
+
+    nynewad = fixlist(nystreetad)
+    nytowns = list()
+    for i in range(len(splittxt)):
+        
+        try:
+            addressidx = splittxt[i].index("Post Office\n")
+            style = splittxt[i].index("© 2020 Hudson")
+        except ValueError:
+            break
+
+        textrange = splittxt[i][addressidx:style]
+
+        adrange = textrange.splitlines()
+        rangeidx = adrange.index("Post Office")
+
+        try:
+            stopidx = adrange.index('')
+        except ValueError:
+            stopidx = len(adrange)
+        for j in adrange[rangeidx:stopidx]:
+            if j == "Post Office":
+                continue
+            nytowns.append(j)
     
-    for j in adrange[rangeidx:stopidx]:
-        if j == "Town":
-             continue
-        towns.append(j)
-print(streetad)
-print(towns)
-with open("test.txt","w") as testing:
-    print(len(towns))
-    print(len(streetad))
-    for i in range(len(streetad)):
-        testing.write(streetad[i])
-        testing.write(" ")
-        testing.write(towns[i])
-        testing.write("\n")
+    print(nynewad)
+    print(nytowns)
+    print(len(nynewad))
+    print(len(nytowns))
 
-#addressidx = text.index("Address\n")
-#style = text.index("Style/Zoning/\n")
-#rental = text.index("Rental Type\n")
+    for ads in nynewad:
+        finalnyad.append(ads)
+    for ftowns in nytowns:
+        finalnytown.append(ftowns)
 
-#addresses = text[addressidx+1:style]
-
-#ad = list()
-
-#for i in addresses:
-#    addressname = i.replace("\n","")
-#    index = addresses.index(i)
-#    lastaddress = addresses[index - 1].replace("\n","")
-#
-#    if i.find(" ") == -1 and len(lastaddress) > 16:
-#         ad[index-1] = lastaddress + i
-#    else:
-#        ad.append(i)
-#
-#for i in ad:
-#    print(i.replace("\n",""))
-
-
-    
+    #os.remove("C:\\Users\\jayma\\OneDrive\\Documents\\GitHub\\WorkParser\\%s" %cmas[readcma])
+"""   
+write_excel(finalad,finaltown)#,finalnyad,finalnytown)
